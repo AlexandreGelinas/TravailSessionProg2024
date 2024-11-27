9f6871dfae202d12d20bb3b9ffb1f159d535877a
QcLoucas <loucasxxv@icloud.com>
2024-11-27 14:14:20 -0500

    Programme qui se lance en admin auto

--- TravailSessionProg2024/TravailSessionProg2024/MainWindow.xaml.cs
+++ TravailSessionProg2024/TravailSessionProg2024/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using TravailSessionProg2024;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -5,8 +6,10 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using MySqlX.XDevAPI;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -27,12 +30,74 @@ namespace TravailSessionProg2024
         {
             this.InitializeComponent();
             GestionWindow.mainWindow = this;
-            //Singleton.getInstance().Ouverture();
+            Singleton.getInstance().Connexion();
         }
 
         private void nvSample_Loaded(object sender, RoutedEventArgs e)
         {
             nvSample.SelectedItem = nvSample.MenuItems[0];
         }
+
+        private void nvSample_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            ObservableCollection<NavigationViewItem> liste = new ObservableCollection<NavigationViewItem>();
+            NavigationViewItem nvi = nvSample.SelectedItem as NavigationViewItem;
+            int index = -1;
+           
+            foreach(NavigationViewItem nvItem in nvSample.MenuItems)
+            {
+                liste.Add(nvItem);
+            }
+            foreach (NavigationViewItem nvItem in nvSample.FooterMenuItems)
+            {
+                liste.Add(nvItem);
+            }
+
+            for (int ctr = 0; ctr < liste.Count(); ctr++)
+            {
+                if (liste[ctr] == nvi)
+                {
+                    index= ctr;
+                }
+            }
+
+            switch (index)
+            {
+                case 0: contentFrame.Navigate(typeof(Activites));
+                    break;
+
+                case 1: contentFrame.Navigate(typeof(Connexion));
+                    break;
+
+                case 2: contentFrame.Navigate(typeof(Admin));
+                    break;
+
+                case 3: contentFrame.Navigate(typeof(Export));
+                    break;
+
+                case 4: contentFrame.Navigate(typeof(Statistique));
+                    break;
+            }
+        }
+
+        public void admin_affichage()
+        {
+            ObservableCollection<NavigationViewItem> liste = new ObservableCollection<NavigationViewItem>();
+
+            foreach (NavigationViewItem nvItem in nvSample.MenuItems)
+            {
+                liste.Add(nvItem);
+            }
+            foreach (NavigationViewItem nvItem in nvSample.FooterMenuItems)
+            {
+                liste.Add(nvItem);
+            }
+
+            foreach (NavigationViewItem nvi in liste)
+            {
+                nvi.Visibility = Visibility.Visible;
+            }
+        }
+
     }
 }
--- TravailSessionProg2024/TravailSessionProg2024/Singleton.cs
+++ TravailSessionProg2024/TravailSessionProg2024/Singleton.cs
@@ -1,208 +1,178 @@
-﻿//using Laboratoire2;
-//using Microsoft.UI.Xaml.Controls;
-//using MySql.Data.MySqlClient;
-//using System;
-//using System.Collections.Generic;
-//using System.Collections.ObjectModel;
-//using System.Diagnostics;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Windows.Media.Protection.PlayReady;
+﻿using TravailSessionProg2024;
+using Microsoft.UI.Xaml.Controls;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Protection.PlayReady;
+using TravailSessionProg2024.Classes;
 
-//namespace Exercice_Multipages
-//{
-//    internal class Singleton
-//    {
-//        ObservableCollection<Produit> liste;
-//        ObservableCollection<Produit> listeRecherche;
-//        static Singleton instance = null;
-//        MySqlConnection con;
+namespace TravailSessionProg2024
+{
+    internal class Singleton
+    {
+        ObservableCollection<Adhérent> liste;
+        static Singleton instance = null;
+        MySqlConnection con;
+        int niveau_permission;
+        Adhérent adhérent_connecter;
+        Administrateur administrateur_connecter;
 
-//        //constructeur de la classe
-//        public Singleton()
-//        {
-//            liste = new ObservableCollection<Produit>();
-//            listeRecherche = new ObservableCollection<Produit>();
-//            con = new MySqlConnection("Server=cours.cegep3r.info;Database=420345ri_gr00002_2375213-loucas-ferron;Uid=2375213;Pwd=2375213;");
-//        }
+        //constructeur de la classe
+        public Singleton()
+        {
+            liste = new ObservableCollection<Adhérent>();
+            con = new MySqlConnection("Server=cours.cegep3r.info;Database=a2024_420-345-ri_eq9;Uid=2375213;Pwd=2375213;");
+        }
 
-//        public async void Ouverture()
-//        {
-//            liste.Clear();
-//            MySqlCommand commande = new MySqlCommand();
-//            commande.Connection = con;
-//            commande.CommandText = "select * from Produit";
-//            con.Open();
-//            MySqlDataReader r = commande.ExecuteReader();
+        public async void Ouverture()
+        {
+            liste.Clear();
+            MySqlCommand commande = new MySqlCommand();
+            commande.Connection = con;
+            commande.CommandText = "select * from Adhérent";
+            con.Open();
+            MySqlDataReader r = commande.ExecuteReader();
 
-//            while (r.Read())
-//            {
-//                string nom = r["nom"].ToString();
-//                string prix = r["prix"].ToString();
-//                string categorie = r["categorie"].ToString();
+            while (r.Read())
+            {
+                string nom = r["nom"].ToString();
+                string prix = r["prix"].ToString();
+                string categorie = r["categorie"].ToString();
 
-//                ajouter(new Produit(nom, prix, categorie));
-//            }
+                //ajouter(new Adhérent(nom, prix, categorie));
+            }
 
-//            r.Close();
-//            con.Close();
-//        }
+            r.Close();
+            con.Close();
+        }
 
-//        //retourne l’instance du singleton
-//        public static Singleton getInstance()
-//        {
-//            if (instance == null)
-//                instance = new Singleton();
+        public void Connexion() //FONCTION DE TEST
+        {
+            GestionWindow.mainWindow.admin_affichage();
+        }
 
-//            return instance;
-//        }
+        public void Connexion(Administrateur user)
+        {
+            GestionWindow.mainWindow.admin_affichage();
+            administrateur_connecter = user;
+        }
 
-//        //retourne la liste des clients
-//        public ObservableCollection<Produit> GetListeProduits()
-//        {
-//            Ouverture();
-//            return liste;
-//        }
+        public void Connexion(Adhérent user)
+        {
+            adhérent_connecter = user;
+        }
 
-//        public ObservableCollection<Produit> GetListeRecherche()
-//        {
-//            return listeRecherche;
-//        }
+        //retourne l’instance du singleton
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+                instance = new Singleton();
 
-//        //retourne un client à une position précise
-//        public Produit getProduit(int position)
-//        {
-//            return liste[position];
-//        }
+            return instance;
+        }
 
-//        public Produit getProduitRercherche(int position)
-//        {
-//            return listeRecherche[position];
-//        }
-
-//        //ajoute un client dans la liste
-//        public void ajouter(Produit Produit)
-//        {
-//            liste.Add(Produit);
-//        }
-
-//        public void ajouterRecherche(Produit Produit)
-//        {
-//            listeRecherche.Add(Produit);
-//        }
-
-//        //modifie un client à une position précise
-//        public void modifier(int position, Produit Produit)
-//        {
-//            liste[position] = Produit;
-//        }
-
-//        //supprime un client à une position précise
-//        public void supprimer(int position)
-//        {
-//            BD_Supprimer(Singleton.getInstance().getProduit(position).Nom);
-//            liste.RemoveAt(position);
-//        }
-
-//        public void supprimerRecherche(int position)
-//        {
-//            BD_Supprimer(Singleton.getInstance().getProduitRercherche(position).Nom);
-//            listeRecherche.RemoveAt(position);
-//        }
-
-//        public bool verification(string nom)
-//        {
-//            Ouverture();
-//            for (int ctr = 0; ctr < liste.Count; ctr++)
-//            {
-//                if (nom.Equals(getProduit(ctr).Nom))
-//                {
-//                    return true;
-//                }
-//            }
-
-//            return false;
-//        }
-
-//        public void BD_Ajouter(Produit item)
-//        {
-//            try
-//            {
-//                MySqlCommand commande = new MySqlCommand();
-//                commande.Connection = con;
-//                commande.CommandText = $"insert into Produit values(\"{item.Nom}\",\"{item.Prix}\",\"{item.Categorie}\")";
-
-//                con.Open();
-//                commande.ExecuteNonQuery();
-
-//                con.Close();
-//                ajouter(item);
-//            }
-//            catch (Exception ex)
-//            {
-//                Debug.WriteLine(ex.ToString());
-//                if (con.State == System.Data.ConnectionState.Open)
-//                {
-//                    con.Close();
-//                }
-//            }
-//        }
-
-//        public void BD_Supprimer(string nom)
-//        {
-//            nom = nom.Replace("'", " ");
-//            try
-//            {
-//                MySqlCommand commande = new MySqlCommand();
-//                commande.Connection = con;
-//                commande.CommandText = $"Delete from Produit WHERE REPLACE(nom, \"'\"  ,  \" \") = '{nom}'";
-
-//                con.Open();
-//                commande.ExecuteNonQuery();
-
-//                con.Close();
-
-//            }
-//            catch (Exception ex)
-//            {
-//                if (con.State == System.Data.ConnectionState.Open)
-//                {
-//                    con.Close();
-//                }
-//            }
-//        }
+        //retourne la liste des clients
+        public ObservableCollection<Adhérent> GetListeAdhérents()
+        {
+            Ouverture();
+            return liste;
+        }
 
 
-//        public void BD_Recherche(string rville)
-//        {
-//            listeRecherche.Clear();
-//            MySqlCommand commande = new MySqlCommand();
-//            commande.Connection = con;
-//            commande.CommandText = $"select * from Produit where ville regexp \"{rville}\"";
-//            con.Open();
-//            MySqlDataReader r = commande.ExecuteReader();
-
-//            while (r.Read())
-//            {
-//                string nom = r["nom"].ToString();
-//                string ville = r["ville"].ToString();
-//                string logo = r["logo"].ToString();
-
-//                for (int i = 0; i < liste.Count(); i++)
-//                {
-//                    if (nom.Equals(liste[i].Nom))
-//                    {
-//                        ajouterRecherche(liste[i]);
-//                    }
-//                }
+        //retourne un client à une position précise
+        public Adhérent getAdhérent(int position)
+        {
+            return liste[position];
+        }
 
 
-//            }
+        //ajoute un client dans la liste
+        public void ajouter(Adhérent Adhérent)
+        {
+            liste.Add(Adhérent);
+        }
 
-//            r.Close();
-//            con.Close();
-//        }
 
-//    }
-//}
+        //modifie un client à une position précise
+        public void modifier(int position, Adhérent Adhérent)
+        {
+            liste[position] = Adhérent;
+        }
+
+        //supprime un client à une position précise
+        //public void supprimer(int position)
+        //{
+        //    BD_Supprimer(Singleton.getInstance().getAdhérent(position).Nom);
+        //    liste.RemoveAt(position);
+        //}
+
+
+        //public bool verification(string nom)
+        //{
+        //    Ouverture();
+        //    for (int ctr = 0; ctr < liste.Count; ctr++)
+        //    {
+        //        if (nom.Equals(getAdhérent(ctr).Nom))
+        //        {
+        //            return true;
+        //        }
+        //    }
+
+        //    return false;
+        //}
+
+        //public void BD_Ajouter(Adhérent item)
+        //{
+        //    try
+        //    {
+        //        MySqlCommand commande = new MySqlCommand();
+        //        commande.Connection = con;
+        //        commande.CommandText = $"insert into Adhérent values(\"{item.Nom}\",\"{item.Prix}\",\"{item.Categorie}\")";
+
+        //        con.Open();
+        //        commande.ExecuteNonQuery();
+
+        //        con.Close();
+        //        ajouter(item);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Debug.WriteLine(ex.ToString());
+        //        if (con.State == System.Data.ConnectionState.Open)
+        //        {
+        //            con.Close();
+        //        }
+        //    }
+        //}
+
+        //public void BD_Supprimer(string nom)
+        //{
+        //    nom = nom.Replace("'", " ");
+        //    try
+        //    {
+        //        MySqlCommand commande = new MySqlCommand();
+        //        commande.Connection = con;
+        //        commande.CommandText = $"Delete from Adhérent WHERE REPLACE(nom, \"'\"  ,  \" \") = '{nom}'";
+
+        //        con.Open();
+        //        commande.ExecuteNonQuery();
+
+        //        con.Close();
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        if (con.State == System.Data.ConnectionState.Open)
+        //        {
+        //            con.Close();
+        //        }
+        //    }
+        //}
+
+    }
+}
