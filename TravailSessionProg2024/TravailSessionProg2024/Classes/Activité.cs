f89836d4cee0882be390e3fc2c5dccd154c3df67
AlexandreGélinas <alexandre.gelinas20@outlook.fr>
2024-11-27 13:02:31 -0500

    Création du document des classes. Classe Admin et Classe Activité fait.

---
+++ TravailSessionProg2024/TravailSessionProg2024/Classes/Activité.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravailSessionProg2024.Classes
+{
+    internal class Activité
+    {
+        public string Nom { get; set; }
+        public string Type { get; set; }
+        public double CoutOrganisation { get; set; }
+        public double PrixVenteParClient { get; set; }
+
+
+        public Activité(string nom, string type, double coutOrganisation, double prixVenteParClient)
+        {
+            Nom = nom;
+            Type = type;
+            CoutOrganisation = coutOrganisation;
+            PrixVenteParClient = prixVenteParClient;
+        }
+
+        public override string ToString()
+        {
+            return $"Nom: {Nom}, Type: {Type}, Cout d'organisation: {CoutOrganisation}" +
+                $" Prix de l'activité: {PrixVenteParClient}";
+        }
+    }
+}
---
+++ TravailSessionProg2024/TravailSessionProg2024/Classes/Administrateur.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravailSessionProg2024.Classes
+{
+    internal class Administrateur
+    {
+        public int ID { get; set; }
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+        public string MotDePasse { get; set; }
+       
+
+
+        public Administrateur(int id, string prenom, string nom, string motDePasse)
+        {
+            
+            ID = id;
+            Prenom = prenom;
+            Nom = nom;  
+            MotDePasse = motDePasse;
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Nom: {Nom}, Prenom: {Prenom}, ID d'admin: {ID}";
+        }
+    }
+}
